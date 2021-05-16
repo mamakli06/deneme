1f94fae611b3484f36cf3161568ea526708cc224
mamakli06 <83599873+mamakli06@users.noreply.github.com>
2021-05-16 21:45:58 +0300

    Create help.py

---
+++ handlers/help.py
@@ -0,0 +1,38 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+
+
+@Client.on_message(
+    filters.command("help")
+    & filters.private
+    & ~ filters.edited
+)
+async def help_(client: Client, message: Message):
+    await message.reply_text(
+        f"""⬇️ nasıl kullanacağını öğretiyorum bı kerede anla⬇️
+- `/oynat` la bebe /oynat yap çalsın uzatma
+- `/bul` gardaş /bul şarkı adi yaz bulur.
+- `/oynat` diğerinin aynısı
+- `/atla` senin anlican diger şarkıya geçiyor
+- `/bitir` müzik biter
+- `/duraklat` durdurur
+- `/devam` devam ettirir anlatırken yoruldum. 
+- Inline search is also supported.""")
+
+@Client.on_message(
+    filters.command("help")
+    & filters.group
+    & ~ filters.edited
+)
+async def help(client: Client, message: Message):
+    await message.reply_text(
+          f"""⬇️ nasıl kullanacağını öğretiyorum bı kerede anla⬇️
+- `/oynat` la bebe /oynat yap çalsın uzatma
+- `/bul` gardaş /bul şarkı adi yaz bulur.
+- `/oynat` diğerinin aynısı
+- `/atla` senin anlican diger şarkıya geçiyor
+- `/bitir` müzik biter
+- `/duraklat` durdurur
+- `/devam` devam ettirir anlatırken yoruldum. 
+- Inline search is also supported.""")
