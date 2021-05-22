746de34ce150670018279fb01066052f075bc712
mamakli06 <83599873+mamakli06@users.noreply.github.com>
2021-05-22 20:32:23 +0300

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -67,31 +67,6 @@ async def generate_cover(requested_by, title, views, duration, thumbnail):
                 await f.write(await resp.read())
                 await f.close()
 
-  
-    image1 = Image.open("./background.png")
-    image2 = Image.open("Mamaklı'nın/thumbs_b_c_b59dfe8ea32ed36641cd18e8595dc8e9.jpg")
-    image3 = changeImageSize(1280, 720, image1)
-    image4 = changeImageSize(1280, 720, image2)
-    image5 = image3.convert("RGBA")
-    image6 = image4.convert("RGBA")
-    Image.alpha_composite(image5, image6).save("temp.png")
-    img = Image.open("temp.png")
-    draw = ImageDraw.Draw(img)
-    font = ImageFont.truetype("Mamaklı'nın/font.otf", 32)
-    draw.text((190, 550), f"Title: {title}", (255, 255, 255), font=font)
-    draw.text(
-        (190, 590), f"Duration: {duration}", (255, 255, 255), font=font
-    )
-    draw.text((190, 630), f"Views: {views}", (255, 255, 255), font=font)
-    draw.text((190, 670),
-        f"Added By: {requested_by}",
-        (255, 255, 255),
-        font=font,
-    )
-    img.save("final.png")
-    os.remove("temp.png")
-    os.remove("background.png")
-
 @Client.on_message(command("oynat") & other_filters)
 @errors
 async def oynat(_, message: Message):
